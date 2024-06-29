4f77945f321d8d1c1894be72c19ff648413c86d0
Jason Rahanetra <jrahanetra@gmail.com>
2024-06-29 09:25:58 +0300

    Ajout du DataManager.java pour la gestion de base de donnée

---
+++ app/src/main/java/mg/geit/jason/DataManager.java
@@ -0,0 +1,76 @@
+package mg.geit.jason;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataManager extends SQLiteOpenHelper {
+    public static final String db_name = "G-PROD8.db";
+    public static final int db_vesrion = 1;
+
+    public DataManager(Context context) {
+        super(context, db_name, null, db_vesrion);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String strSql = "Create TABLE ProduitCategorie (" +
+                "id_Categorie integer primary key autoincrement, " +
+                "name_Categorie text not null " +
+                ")";
+        String newStrSql = "Create Table Produits  (" +
+                "id_Produit integer primary key autoincrement," +
+                "name_Produit text not null," +
+
+                "description_Produit text not null,"+
+                "id_Categorie integer,"+
+                "foreign key(id_Categorie) references ProduitCategorie(id_Categorie)" +
+                ")";
+        db.execSQL(strSql);
+        db.execSQL(newStrSql);
+        Log.i("DataBase", "onCreate invoked");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    public void insertProduit(String name, String description, Integer id_Produit)
+    {
+        String strSql = "Insert into Produits (name_Produit, description_Produit,id_Categorie) values" +
+                "('"+name+"','"+description+"','"+id_Produit+"')";
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL(strSql);
+        Log.i("DataBase", "insertProduit invoked ");
+    }
+
+    public  void insertCatProduit(String name_Cat){
+        String strSql = "Insert into ProduitCategorie (name_Categorie) values"+
+                "('"+name_Cat+"')";
+        SQLiteDatabase  db = this.getWritableDatabase();
+        db.execSQL(strSql);
+        Log.i("DataBase", "insertCatégories invoked ");
+    }
+
+    public List<Category> readCategories(){
+        List<Category> listCat = new ArrayList<>();
+
+        String strSql = "Select * from ProduitCategorie";
+        Cursor cursor = this.getReadableDatabase().rawQuery(strSql, null);
+        cursor.moveToFirst();
+
+        while(! cursor.isAfterLast())
+        {
+            Category categoryData = new Category(cursor.getString(1));
+            listCat.add(categoryData);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return listCat;
+    }
+}
