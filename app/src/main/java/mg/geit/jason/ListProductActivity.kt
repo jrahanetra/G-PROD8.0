6e37218d112f91717a248b53261ba98785f30b3c
Jason Rahanetra <jrahanetra@gmail.com>
2024-06-29 21:20:57 +0300

    Modification et mise en forme de l'IU

--- app/src/main/java/mg/geit/jason/ListProductActivity.kt
+++ app/src/main/java/mg/geit/jason/ListProductActivity.kt
@@ -9,17 +9,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,6 +34,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -41,12 +49,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import mg.geit.jason.ui.theme.GPROD80Theme
 
-class ListProduitActivity : ComponentActivity() {
+class ListProductActivity : ComponentActivity() {
     private var dataManager = DataManager(this)
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,12 +63,12 @@ class ListProduitActivity : ComponentActivity() {
         val idCategory = intent.getStringExtra("IdCategory")
         Log.i("Debug", "$idCategory")
         val id = idCategory?.toInt()
-        val produits = dataManager.readProduits(id)
+        val products = dataManager.readProduits(id)
         enableEdgeToEdge()
         setContent {
             GPROD80Theme {
-                Log.i("ListData", "$produits")
-                MainScreen2(produits)
+                Log.i("ListData", "$products")
+                MainScreen2(products)
             }
         }
     }
@@ -73,7 +82,7 @@ fun MainScreen2(list: List<Produit>){
     Scaffold (
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            Header(title = "Listes")
+            Header(title = "Lists")
         } ,
         floatingActionButton = {
             FloatingActionButton(
@@ -84,42 +93,63 @@ fun MainScreen2(list: List<Produit>){
             }
         },
     ){  innerPadding ->
-        ScrollDataProduit(list, innerPadding)
+        ScrollDataProduct(list, innerPadding)
     }
 }
 
 @Composable
-fun ScrollDataProduit(list: List<Produit>, innerPadding : PaddingValues){
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(1),
-        contentPadding = innerPadding,
-        modifier = Modifier.background(color = Color.White),
+fun ScrollDataProduct(list: List<Produit>, innerPadding : PaddingValues){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        items(list){ produit ->
-            run {
-                ShowProduit(produit)
-            }
-        }
-        items(list){ produit ->
-            run {
-                ShowProduit(produit)
-            }
-        }
-        items(list){ produit ->
-            run {
-                ShowProduit(produit)
-            }
-        }
-        items(list){ produit ->
-            run {
-                ShowProduit(produit)
+        Spacer(modifier = Modifier.height(0.dp)) // Ajoute un espace de 16dp entre la topBar et le TextFieldWithIcons
+        TextFieldWithIcons()
+        Spacer(modifier = Modifier.height(15.dp)) // Ajoute un espace de 16dp entre la topBar et le TextFieldWithIcons
+        OutlinedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White,
+            ),
+            border = BorderStroke(1.dp, Color.Black), // Bordure transparente
+            modifier = Modifier
+                .size(width = 350.dp, height = 600.dp)
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(1),
+                modifier = Modifier
+                    .background(color = Color.White)
+                    .fillMaxHeight(),
+            ) {
+                items(list) { product ->
+                    run {
+                        ShowProduct(product)
+                    }
+                }
+                items(list) { product ->
+                    run {
+                        ShowProduct(product)
+                    }
+                }
+                items(list) { product ->
+                    run {
+                        ShowProduct(product)
+                    }
+                }
+                items(list) { product ->
+                    run {
+                        ShowProduct(product)
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun ShowProduit(produit: Produit){
+fun ShowProduct(product: Produit){
     var checked by remember { mutableStateOf(true) }
     OutlinedCard(
         colors = CardDefaults.cardColors(
@@ -127,7 +157,7 @@ fun ShowProduit(produit: Produit){
         ),
         border = BorderStroke(0.dp, Color.Transparent), // Bordure transparente
         modifier = Modifier
-            .size(width = 900.dp, height = 50.dp)
+            .size(width = 600.dp, height = 50.dp)
     ){
         Row(
             modifier = Modifier
@@ -144,7 +174,7 @@ fun ShowProduit(produit: Produit){
                         .size(30.dp)
                 )
                 Text(
-                    text = produit.name,
+                    text = product.name,
                     modifier = Modifier
                         .fillMaxWidth(),
                     color = Color.Black,
@@ -154,10 +184,26 @@ fun ShowProduit(produit: Produit){
         }
     }
 }
+
+@Composable
+fun TextFieldWithIcons() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        value = text,
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "SearchIcon") },
+        //trailingIcon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) },
+        onValueChange = {
+            text = it
+        },
+        modifier = Modifier.width(350.dp),
+        label = { Text(text = "NomProduit") },
+        placeholder = { Text(text = "Entrer le nom du produit") },
+    )
+}
 @Preview (showBackground = true)
 @Composable
 fun Preview2() {
     GPROD80Theme {
-        ShowProduit(produit = Produit("mon produitlk hghuyh", 100, 200, "Rien à montrer pour l'instant"))
+        ShowProduct(product = Produit("mon produit", 100, 200, "Rien à montrer pour l'instant"))
     }
 }
