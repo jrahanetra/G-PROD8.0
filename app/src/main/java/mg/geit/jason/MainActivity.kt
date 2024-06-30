bf6ec73f46a7f6238aaaea0c4dfd4bf7f7a78604
Jason Rahanetra <jrahanetra@gmail.com>
2024-06-30 09:00:52 +0300

    Ajout de qlq paramètre nécessaires pour les actions

--- app/src/main/java/mg/geit/jason/MainActivity.kt
+++ app/src/main/java/mg/geit/jason/MainActivity.kt
@@ -13,22 +13,28 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -53,15 +59,21 @@ class MainActivity : ComponentActivity() {
     private var dataManager = DataManager(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        insertionData(dataManager)
+//        insertionData(dataManager)
         enableEdgeToEdge()
         setContent {
             GPROD80Theme {
-                MainScreen1(dataManager, seeListActivity =  { category ->
-                    val intent = Intent(this, ListProductActivity::class.java).apply { putExtra("IdCategory", "${category.id}")}
-                    Log.i("clickCard","CardClické: ${category.name}")
-                    startActivity(intent)
-                })
+                MainScreen1(
+                    dataManager,
+                    seeListActivity =  { categorie ->
+                        val intent = Intent(this, ListProductActivity::class.java)
+                                        .apply { putExtra("IdCategory", categorie.id)}
+                        Log.i("clickCard","CardClické: ${categorie.name}")
+                        startActivity(intent)
+                    },
+                    produit = Produit(null,"",null,null,"null"),
+                    doModification = {Log.i("Debug", "doModification INVOKED")},
+                )
             }
         }
     }
@@ -69,64 +81,78 @@ class MainActivity : ComponentActivity() {
 
 fun insertionData(dataManager: DataManager){
     Log.i("insertions", "Insertion data invoked")
-    dataManager.insertCatProduit("NOURRITURE", R.drawable.vector)
-    dataManager.insertCatProduit("FOURNITURE", R.drawable.fourniture)
-    dataManager.insertCatProduit("ELECTRONIQUE", R.drawable.electronique)
-    dataManager.insertCatProduit("MENAGER", R.drawable.menager)
-    dataManager.insertCatProduit("SOIN", R.drawable.ic_launcher_foreground)
+    dataManager.insertCatProduct("NOURRITURE", R.drawable.vector)
+    dataManager.insertCatProduct("FOURNITURE", R.drawable.fourniture)
+    dataManager.insertCatProduct("ELECTRONIQUE", R.drawable.electronique)
+    dataManager.insertCatProduct("MENAGER", R.drawable.menager)
+    dataManager.insertCatProduct("SOIN", R.drawable.ic_launcher_foreground)
     // Produits pour la catégorie "NOURRITURE" (catégorie ID = 1)
-    dataManager.insertProduit("Burger", 400, 20, "C est un fast food", 1)
-    dataManager.insertProduit("Pizza", 500, 30, "Délicieux et gourmand", 1)
-    dataManager.insertProduit("Gâteau", 1000, 10, "Sucré et délicieux, parfait pour un anniversaire", 1)
-    dataManager.insertProduit("Sandwich", 250, 50, "Rapide et pratique pour le déjeuner", 1)
+    dataManager.insertProduct("Burger", 400, 20, "C est un fast food", 1)
+    dataManager.insertProduct("Pizza", 500, 30, "Délicieux et gourmand", 1)
+    dataManager.insertProduct("Gâteau", 1000, 10, "Sucré et délicieux, parfait pour un anniversaire", 1)
+    dataManager.insertProduct("Sandwich", 250, 50, "Rapide et pratique pour le déjeuner", 1)
 
     // Produits pour la catégorie "FOURNITURE" (catégorie ID = 2)
-    dataManager.insertProduit("Cahier", 20, 500, "Solide et durable, idéal pour les étudiants", 2)
-    dataManager.insertProduit("Stylo", 5, 1000, "Écriture fluide, disponible en plusieurs couleurs", 2)
-    dataManager.insertProduit("Sac à dos", 1500, 100, "Confortable et spacieux pour transporter vos affaires", 2)
+    dataManager.insertProduct("Cahier", 20, 500, "Solide et durable, idéal pour les étudiants", 2)
+    dataManager.insertProduct("Stylo", 5, 1000, "Écriture fluide, disponible en plusieurs couleurs", 2)
+    dataManager.insertProduct("Sac à dos", 1500, 100, "Confortable et spacieux pour transporter vos affaires", 2)
 
     // Produits pour la catégorie "ELECTRONIQUE" (catégorie ID = 3)
-    dataManager.insertProduit("Téléphone", 30000, 15, "Smartphone dernière génération avec écran OLED", 3)
-    dataManager.insertProduit("Ordinateur portable", 70000, 10, "Puissant et léger, parfait pour le travail et les loisirs", 3)
-    dataManager.insertProduit("Casque audio", 2000, 50, "Son de haute qualité avec réduction de bruit", 3)
+    dataManager.insertProduct("Téléphone", 30000, 15, "Smartphone dernière génération avec écran OLED", 3)
+    dataManager.insertProduct("Ordinateur portable", 70000, 10, "Puissant et léger, parfait pour le travail et les loisirs", 3)
+    dataManager.insertProduct("Casque audio", 2000, 50, "Son de haute qualité avec réduction de bruit", 3)
 
     // Produits pour la catégorie "MENAGER" (catégorie ID = 4)
-    dataManager.insertProduit("Aspirateur", 5000, 20, "Efficace pour nettoyer tous types de sols", 4)
-    dataManager.insertProduit("Machine à laver", 25000, 5, "Capacité de 7 kg avec plusieurs modes de lavage", 4)
-    dataManager.insertProduit("Réfrigérateur", 40000, 8, "Grand espace de rangement avec congélateur intégré", 4)
+    dataManager.insertProduct("Aspirateur", 5000, 20, "Efficace pour nettoyer tous types de sols", 4)
+    dataManager.insertProduct("Machine à laver", 25000, 5, "Capacité de 7 kg avec plusieurs modes de lavage", 4)
+    dataManager.insertProduct("Réfrigérateur", 40000, 8, "Grand espace de rangement avec congélateur intégré", 4)
 
     // Produits pour la catégorie "SOIN" (catégorie ID = 5)
-    dataManager.insertProduit("Shampooing", 150, 100, "Pour des cheveux propres et soyeux", 5)
-    dataManager.insertProduit("Crème hydratante", 300, 50, "Hydrate et nourrit la peau en profondeur", 5)
-    dataManager.insertProduit("Dentifrice", 50, 200, "Protection complète pour des dents saines et blanches", 5)
+    dataManager.insertProduct("Shampooing", 150, 100, "Pour des cheveux propres et soyeux", 5)
+    dataManager.insertProduct("Crème hydratante", 300, 50, "Hydrate et nourrit la peau en profondeur", 5)
+    dataManager.insertProduct("Dentifrice", 50, 200, "Protection complète pour des dents saines et blanches", 5)
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen1(dataManager: DataManager, seeListActivity: (Category) -> Unit) {
+fun MainScreen1(dataManager: DataManager, seeListActivity: (Category) -> Unit, produit: Produit, doModification: (Produit) -> Unit) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-
         topBar = {
-            Header(title = "Catégories")
+            Header(
+                title = "Catégories",
+                false,
+                produit,
+                doModification
+            )
         },
         floatingActionButton = {
-            FloatingActionButton(
-                onClick = { /* TODO */ },
-                containerColor = colorResource(id = R.color.color1),// Couleur de fond personnalis
-                ) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
+            ShowFloatingActionButton()
         },
     ) { innerPadding ->
         ScrollContent(dataManager, innerPadding, seeListActivity)
     }
 }
 
+@Composable
+fun ShowFloatingActionButton(){
+    FloatingActionButton(
+        onClick = { /* TODO */ },
+        containerColor = colorResource(id = R.color.color1),// Couleur de fond personnalis
+        shape = FloatingActionButtonDefaults.largeShape,
+        modifier = Modifier.padding(0.dp,0.dp,8.dp,0.dp)
+    ) {
+        Icon(
+            Icons.Default.Add,
+            contentDescription = "Ajouter",
+            modifier = Modifier.size(50.dp)
+        )
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Header(title: String){
+fun Header(title: String, editable: Boolean, produit: Produit, doModification:(Produit)->Unit){
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     Column {
         CenterAlignedTopAppBar(
@@ -165,30 +191,64 @@ fun Header(title: String){
                 .align(Alignment.CenterHorizontally),
             color = colorResource(id = R.color.color1),
             contentColor = colorResource(id = R.color.white),
-            content = { TitleSection(
-                title = title,
-                modifier = Modifier.padding(5.dp),
-                textAlign = TextAlign.Center,
-            ) },
+            content = {
+                TitleSection(
+                    title = title,
+                    modifier = Modifier
+                        .padding(5.dp),
+                    textAlign = TextAlign.Center,
+                    editable,
+                    produit,
+                    doModification
+                )
+            }
         )
     }
 }
+
 @Composable
-fun TitleSection(title: String, modifier: Modifier = Modifier, textAlign: TextAlign){
-    Text(
-        text = title,
-        modifier = modifier,
-        textAlign = textAlign
-    )
+fun TitleSection(title: String, modifier: Modifier = Modifier, textAlign: TextAlign, editable: Boolean, produit: Produit, doModification: (Produit) -> Unit){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(40.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Row(
+        ){
+            Text(
+                text = title,
+                modifier = modifier
+                    .width(320.dp)
+                    .height(25.dp),
+                textAlign = textAlign
+            )
+            if (editable){
+                IconButton(
+                    onClick = { doModification(produit)},
+                    modifier = Modifier.height(30.dp)
+                ) {
+                    Icon(
+                        Icons.Default.Edit,
+                        contentDescription = "Editer Produit",
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
 fun ScrollContent(dataManager: DataManager, innerPadding: PaddingValues, seeListActivity: (Category) -> Unit) {
-    val categories = dataManager.readCategories()
+    val categories = dataManager.readCategory()
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         contentPadding = innerPadding,
-        modifier = Modifier.background(color = Color.White)
+        modifier = Modifier
+            .background(color = Color.White)
             .fillMaxHeight()
             .fillMaxWidth()
             .fillMaxSize()
@@ -226,6 +286,7 @@ fun CategoryCard(category: Category, seeListActivity: (Category) -> Unit) {
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun Preview(){
