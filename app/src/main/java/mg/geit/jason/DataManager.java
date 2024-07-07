866cc8905082ff77c3534a725a29164275a90a4d
Jason Rahanetra <jrahanetra@gmail.com>
2024-07-07 11:12:59 +0300

    Add kotlin doc and update methods to get idCategory

--- app/src/main/java/mg/geit/jason/DataManager.java
+++ app/src/main/java/mg/geit/jason/DataManager.java
@@ -16,6 +16,10 @@ public class DataManager extends SQLiteOpenHelper {
         super(context, db_name, null, db_vesrion);
     }
 
+    /**
+     * CREATE THE BASE AND ALL TABLES IN THIS FUNCTION
+     * @param db The database.
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         String strSql = "Create TABLE ProduitCategorie (" +
@@ -43,6 +47,15 @@ public class DataManager extends SQLiteOpenHelper {
 
     }
 
+    /**
+     * FUNCTION TO INSERT PRODUCT IN A CATEGORY
+     * @param name the name of the product to insert
+     * @param prix the price of the product to insert
+     * @param qtt the quantity of the product to insert
+     * @param description the description of the product to insert
+     * @param imageUrl the image url of the product to insert
+     * @param id_CatProduit the id of the catgory that this product belong to
+     */
     public void insertProduct(String name, int prix, int qtt, String description, String imageUrl, int id_CatProduit)
     {
         // Échapper les apostrophes
@@ -56,7 +69,17 @@ public class DataManager extends SQLiteOpenHelper {
         Log.i("DataBase", "insertProduit invoked ");
     }
 
-    public void updateProduct(int id, String name, int prix, int qtt, String imageUrl, String description) {
+    /**
+     * FUNCTION TO UPDATE PRODUCT
+     * @param id the id of the product
+     * @param name the name of the product
+     * @param prix the price of the product
+     * @param qtt the quantity of the product
+     * @param imageUrl the url image of the product
+     * @param description the description of the product
+     */
+    public void updateProduct(int id, String name, int prix, int qtt, String imageUrl, String description)
+    {
         String strSql = "UPDATE Produits SET " +
                 "name_Produit = '" + name + "', " +
                 "prix_Produit = " + prix + ", " +
@@ -70,7 +93,13 @@ public class DataManager extends SQLiteOpenHelper {
         Log.i("DataBase", "updateProduct invoked for id_Produit: " + id);
     }
 
-    public  void insertCatProduct(String name_Cat, String image_CatUrl){
+    /**
+     * FUNCTION TO INSERT DATA PRODUCT
+     * @param name_Cat
+     * @param image_CatUrl
+     */
+    public  void insertCatProduct(String name_Cat, String image_CatUrl)
+    {
         String strSql = "Insert into ProduitCategorie (name_Categorie, image_CategorieUrl) values"+
                 "('"+name_Cat+"','"+image_CatUrl+"')";
         SQLiteDatabase  db = this.getWritableDatabase();
@@ -78,7 +107,12 @@ public class DataManager extends SQLiteOpenHelper {
         Log.i("DataBase", "insertCatégories invoked ");
     }
 
-    public List<Category> readCategory(){
+    /**
+     * FUNCTION TO READCATORY
+     * @return List<Category>
+     */
+    public List<Category> readCategory()
+    {
         List<Category> listCat = new ArrayList<>();
 
         String strSql = "Select * from ProduitCategorie";
@@ -95,7 +129,13 @@ public class DataManager extends SQLiteOpenHelper {
         return listCat;
     }
 
-    public List<Produit> readProducts(Integer id){
+    /**
+     * FUNCTION TO READ ALL PRODUCTS
+     * @param id
+     * @return List<Category>
+     */
+    public List<Produit> readProducts(Integer id)
+    {
         List<Produit> listProduit = new ArrayList<>();
 
         String strSql = "Select * from Produits where id_Categorie = "+id+";";
@@ -103,7 +143,7 @@ public class DataManager extends SQLiteOpenHelper {
         cursor.moveToFirst();
 
         while(! cursor.isAfterLast()){
-            Produit produit = new Produit(cursor.getInt(0),cursor.getString(1), cursor.getInt(2), cursor.getInt(3),cursor.getString(5), cursor.getString(4));
+            Produit produit = new Produit(cursor.getInt(0),cursor.getString(1), cursor.getInt(2), cursor.getInt(3),cursor.getString(5), cursor.getString(4), cursor.getInt(6));
             listProduit.add(produit);
             cursor.moveToNext();
         }
@@ -111,14 +151,20 @@ public class DataManager extends SQLiteOpenHelper {
         return listProduit;
     }
 
-    public Produit readProduct(Integer idProduct){
+    /**
+     * FUNCTION TO READ ONE SPECIFY PRODUCT BY HIS ID
+     * @param idProduct
+     * @return Produit
+     */
+    public Produit readProduct(Integer idProduct)
+    {
         String strSql = "Select * from Produits where id_Produit = "+idProduct;
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(strSql, null);
         Produit produit = null;
         if(cursor != null && cursor.moveToFirst())
             // Le curseur contient des données, on peut les lire
-            produit = new Produit(cursor.getInt(0),cursor.getString(1), cursor.getInt(2), cursor.getInt(3), cursor.getString(5), cursor.getString(4));
+            produit = new Produit(cursor.getInt(0),cursor.getString(1), cursor.getInt(2), cursor.getInt(3), cursor.getString(5), cursor.getString(4), cursor.getInt(6));
         else
             Log.i("Database","le curseur ne contient pas de donnee");
 
@@ -127,5 +173,20 @@ public class DataManager extends SQLiteOpenHelper {
             cursor.close();
         db.close();
         return  produit;
+    }
+
+    /**
+     * TO VERIFY IF TABLE CATEGORY ARE EMPTY OR NO
+     * @return boolean
+     */
+    public boolean isCategoryTableEmpty()
+    {
+        String strSql = "SELECT COUNT(*) FROM ProduitCategorie";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(strSql, null);
+        cursor.moveToFirst();
+        int count = cursor.getInt(0);
+        cursor.close();
+        return count == 0;
     }
 }
